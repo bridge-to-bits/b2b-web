364a55cd6d11147c9ff91da5b4d1563a851dd900
sifes <lonfark@gmail.com>
2024-11-24 13:06:49 +0200

    add adaptive for header

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -3,7 +3,7 @@
 import { FC } from 'react';
 import AuthProvider from '@/lib/providers/auth-provider';
 import { Toaster } from '@/components/ui/toast/toaster';
-import { Header } from '@/components/layout/components/Header';
+import { Header } from '@/components/layout/header/Header';
 import { QueryClientProvider } from '@/lib/providers/query-client-provider';
 import { Footer } from '@/components/layout/components/Footer';
 
---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { LogoIcon } from './Logo';
+import { HeaderSearch } from './HeaderSearch';
+import { useEffect, useState } from 'react';
+import { MobileMenu } from './MobileMenu';
+import { DecktopMenu } from './DecktopMenu';
+
+export const Header = () => {
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth < 1024);
+    const handleResize = () => setIsMobile(window.innerWidth < 1024);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <header className='bg-background-darker fixed z-10 w-full'>
+      <div className='mx-auto flex h-full max-w-[1280px] items-center justify-between gap-8 px-4 py-1'>
+        <div className='flex w-full items-center gap-4 md:gap-8 lg:max-w-[40%]'>
+          <LogoIcon />
+
+          <HeaderSearch />
+        </div>
+        {isMobile ? <MobileMenu /> : <DecktopMenu />}
+      </div>
+    </header>
+  );
+};
