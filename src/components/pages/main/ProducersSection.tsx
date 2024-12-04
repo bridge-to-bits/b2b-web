de0b6f2d7b82f68a2b58b6b90ab210cba577b3a2
Dmytro Kyrychenko <dimakirimail@gmail.com>
2024-12-04 11:34:26 +0200

    a key should be unique in sections

--- src/components/pages/main/ProducersSection.tsx
+++ src/components/pages/main/ProducersSection.tsx
@@ -34,7 +34,10 @@ export const ProducersSection: React.FC = () => {
       {/* Producers List */}
       <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-[20px] mt-[20px] px-[5%]'>
         {producers?.map((producer) => (
-          <ProducerCard key={producer.userName} producer={producer} />
+          <ProducerCard
+            key={producer.userName + producer.email}
+            producer={producer}
+          />
         ))}
       </div>
 
