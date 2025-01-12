3d79a4ff7c736fb95839ab73248c955345cc905d
sifes <lonfark@gmail.com>
2025-01-12 09:53:58 -0800

    feat: add remove from liked in library

--- src/app/(application)/library/page.tsx
+++ src/app/(application)/library/page.tsx
@@ -49,7 +49,7 @@ const Page: FC<PageProps> = async ({ searchParams: { playedSongId } }) => {
     <section className='my-10'>
       <SidebarProvider className='items-start min-h-[75vh]'>
         <LibrarySidebar performers={performers} />
-        <LibrarySongsList songs={tracks} performers={performers} />
+        <LibrarySongsList userId={id} songs={tracks} performers={performers} />
       </SidebarProvider>
     </section>
   );
--- src/components/pages/library/library-songs-list.tsx
+++ src/components/pages/library/library-songs-list.tsx
@@ -1,17 +1,19 @@
 'use client';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { useIsMobile } from '@/hooks/use-mobile';
-import { BookHeart, Heart } from 'lucide-react';
+import { BookHeart } from 'lucide-react';
 import { FC } from 'react';
 
 import { FavoritePerformer } from '@/app/api/performers/performers-api-types';
 import { FavoriteTrack } from '@/app/api/tracks/tracks-api-types';
 import { LibraryPerformersList } from './performers-list';
+import { SongItem } from './SongItem';
 
 export const LibrarySongsList: FC<{
   songs: FavoriteTrack[];
   performers: FavoritePerformer[];
-}> = ({ songs, performers }) => {
+  userId: string;
+}> = ({ songs, performers, userId }) => {
   const isMobile = useIsMobile();
 
   return (
@@ -31,28 +33,13 @@ export const LibrarySongsList: FC<{
       )}
 
       <div className='space-y-4'>
-        {songs.map((song) => (
-          <div key={song.id} className='flex items-center'>
-            <div className='flex items-center justify-between bg-gray-800 p-4  rounded-lg w-full'>
-              <div className='flex flex-col mr-2 max-w-[50%] w-full'>
-                <span className='text-orange text-2xl font-semibold text-nowrap'>
-                  {song.name}
-                </span>
-                <span className='text-white text-xl font-semibold'>
-                  {song.description}
-                </span>
-              </div>
-
-              <iframe
-                src={song.url}
-                className='w-full h-20'
-                style={{
-                  borderRadius: '10px',
-                }}
-              ></iframe>
-            </div>
-            <Heart className='cursor-pointer text-gray-400 hover:text-orange ml-3' />
-          </div>
+        {songs.map((props) => (
+          <SongItem
+            allSongs={songs}
+            key={props.id}
+            {...props}
+            userId={userId}
+          />
         ))}
       </div>
     </div>
