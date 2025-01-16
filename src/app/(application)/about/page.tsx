979cd0d75888305ea7e02d421484f628b49eb484
ValeriiOdnosum <142837725+ValeriiOdnosum@users.noreply.github.com>
2025-01-16 02:21:18 +0200

    about is changed

--- src/app/(application)/about/page.tsx
+++ src/app/(application)/about/page.tsx
@@ -4,16 +4,16 @@ import NextImage from 'next/image';
 
 const About: React.FC = () => {
   return (
-    <div className="w-full h-[3249px] max-w-[1920px] mx-auto">
+    <div className="w-full min-h-screen max-w-[1920px] mx-auto overflow-hidden">
       {/* Banner Section */}
-      <div className="flex flex-col sm:flex-row items-center mt-0 justify-center">
+      <div className="flex flex-col sm:flex-row items-center mt-16 justify-center">
         <div className="relative w-full sm:w-[810px] h-[300px] sm:h-[492px]">
-          <NextImage src='/Rectangle 62.png' alt="Banner" className="w-full h-full object-cover" width={50} height={50}/>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-[24px] sm:text-[48px] lg:text-[96px] font-rubik">
+          <NextImage src='/notes.png' alt="Banner" className="w-full h-full object-cover" width={50} height={50}/>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[80%] text-white text-[24px] sm:text-[48px] lg:text-[66px] font-rubik italic">
             Про нас
           </div>
         </div>
-        <div className="w-full sm:w-[971px] h-auto text-[16px] sm:text-[18px] lg:text-[36px] font-rubik mt-5 sm:mt-0 sm:ml-5 px-4 sm:px-0">
+        <div className="w-full sm:w-[971px] h-auto text-[16px] sm:text-[18px] lg:text-[36px] font-rubik mt-4 sm:mt-0 sm:ml-5 px-4 sm:px-0 ">
           <p>
             Ласкаво просимо на Beat to Beat – платформу, де музиканти та продюсери зустрічаються,
             взаємодіють і створюють. Ми віримо, що справжня музична магія виникає, коли талант знаходить свою можливість.
@@ -24,7 +24,7 @@ const About: React.FC = () => {
       </div>
 
       {/* Intro Paragraph */}
-      <div className="w-full mx-auto mt-20 mb-5 px-4 sm:px-0">
+      <div className="w-full mx-auto mt-20  px-4 sm:px-0 mt-10">
         <p className="text-[16px] sm:text-[18px] lg:text-[36px] font-rubik">
           Beat to Beat – це не просто платформа для знайомств.
           Це спільнота інноваторів, артистів і візіонерів,
@@ -35,7 +35,7 @@ const About: React.FC = () => {
       </div>
 
       {/* Second Banner */}
-      <div className="flex flex-col sm:flex-row items-center mt-[50px] sm:mt-[150px] justify-center">
+      <div className="flex flex-col sm:flex-row items-center mt-10 sm:mt-[150px] justify-center">
         <div className="w-full sm:w-[950px] text-[16px] sm:text-[18px] lg:text-[36px] font-rubik  sm:ml-0 px-4 sm:px-0">
           <p>
             Наша команда прагне забезпечити зручний і простий у користуванні досвід,
@@ -44,42 +44,42 @@ const About: React.FC = () => {
             швидким і зручним. З нами співпраця – це лише один клік.
           </p>
         </div>
-        <div className="w-full sm:w-[950px] h-auto sm:h-[608px] mt-5 sm:mt-0">
-          <NextImage src='/Rectangle 63.png' alt="Second Banner" className="w-full h-full object-cover" width={100} height={100}/>
+        <div className="w-full sm:w-[950px] h-auto sm:h-[608px] sm:mt-0">
+          <NextImage src='/music-studio.png' alt="Second Banner" className="w-full h-full object-cover" width={100} height={100}/>
         </div>
       </div>
 
       {/* Mission Section */}
-      <div className="flex flex-col items-center mt-16 px-4 sm:px-0">
-  {/* Заголовок "Наша місія" */}
-  <div className="w-full sm:w-[1552px] text-[24px] sm:text-[48px] lg:text-[96px] font-rubik mt-1 text-right">
-    Наша місія
+   
+
+
+{/* Vision Section flex flex-col  mt-6 px-4 sm:px-0 ml-10*/}
+<div className="flex flex-col items-center mt-12 mb-16 px-4 ml-10">
+<div className="w-full sm:w-[1552px] text-[24px] sm:text-[48px] lg:text-[66px] font-rubik italic mt-14 mr-5 text-right ">
+  Наша місія
+</div>
+<div className="w-full sm:w-[1638px] text-[16px] sm:text-[18px] lg:text-[36px] font-rubik mt-5 ml-10 md:ml-0 lg:ml-5 sm:ml-5 text-center ">
+  Об’єднувати музикантів і продюсерів, надихаючи на співпрацю, що руйнує кордони й створює звук майбутнього.
+</div>
+  <div className="w-full sm:w-[1552px] sm:p4 text-[24px] sm:text-[48px] lg:text-[66px] font-rubik italic ml-0 mt-10 sm:ml-5 text-left">
+    Наша візія
   </div>
-  {/* Описание "Об’єднувати музикантів і продюсерів..." */}
-  <div className="w-full sm:w-[1638px] text-[16px] sm:text-[18px] lg:text-[36px] font-rubik mt-5 ml-0 sm:ml-5 text-center">
-    Об’єднувати музикантів і продюсерів, надихаючи на співпрацю, що руйнує кордони й створює звук майбутнього.
+  <div className="w-full sm:w-[1638px] text-[16px] sm:text-[18px] lg:text-[36px] font-rubik mt-5 ml-10 sm:ml-5 text-center ">
+    Створити найяскравішу світову спільноту для музичних професіоналів, де кожен артист має свободу співпрацювати, творити і 
+    ділитися своїм унікальним звуком зі світом.
   </div>
 </div>
-      {/* Vision Section */}
-      <div className="flex flex-col  mt-16 px-4 sm:px-0">
-        <div className="w-full sm:w-[1052px] text-[24px] sm:text-[48px] lg:text-[96px] font-rubik ml-0 sm:ml-5 text-left">Наша візія</div>
-        <div className="w-full sm:w-[1638px] text-[16px] sm:text-[18px] lg:text-[36px] font-rubik mt-5 ml-0 sm:ml-5 text-center">
-          Створити найяскравішу світову спільноту для музичних професіоналів,
-          де кожен артист має свободу співпрацювати, творити і ділитися своїм унікальним звуком зі світом.
-        </div>
-      </div>
 
-      {/* Join Us Section */}
-      <div className="flex flex-col items-center mt-16" >
-        <div className="w-full sm:w-[1132px] text-[24px] sm:text-[48px] lg:text-[96px] font-rubik-italic text-center">
-          Приєднуйтеся до нас
-        </div>
-        <div className="w-full sm:w-[1638px] text-[16px] sm:text-[18px] lg:text-[36px] font-rubik mt-16 text-center">
-          Готові створювати музику? Приєднуйтеся до Beat to Beat і
-          відкрийте для себе світ музичних можливостей.
-          Давайте творити музику разом!
-        </div>
-      </div>
+{/* Join Us Section */}
+<div className="flex flex-col items-center mt-12 mb-16 px-4 ml-10">
+  <div className="w-full sm:w-[1132px] text-[24px] sm:text-[48px] lg:text-[66px] font-rubik italic text-center mt-10">
+    Приєднуйтеся до нас
+  </div>
+  <div className="w-full sm:w-[1638px] text-[16px] sm:text-[18px] lg:text-[36px] font-rubik  text-center">
+    Готові створювати музику? Приєднуйтеся до Beat to Beat і відкрийте для себе світ музичних можливостей. Давайте творити музику разом!
+  </div>
+</div>
+
     </div>
   );
 }
